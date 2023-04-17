5349c0720cce5c7bd9189242812f026bca4abbcd
JoaquinWald <joaquingrunwald@gmail.com>
2023-04-17 15:28:05 -0300

    navbar and sidebar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
 
 const drawerWidth = 240;
@@ -8,15 +8,16 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
 
-      {/* NavBar */}
+      <NavBar drawerWidth={drawerWidth} />
 
-      {/* SideBar */}
+      <SideBar drawerWidth={drawerWidth} />
 
       <Box
         component="main"
         sx={{ flexGrow: 1, p: 3 }}
       >
         {/* ToolBar */}
+        <Toolbar />
 
         {children}
 
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,6 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 
-
 export const JournalPage = () => {
   return (
     <JournalLayout>
